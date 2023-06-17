53f02c9f73512136e2553f82148eab8d6b7b3cc3
George kuznecov <georgijkuznecov987@gmail.com>
2023-06-18 01:23:14 +0300

    ошибка

--- part-2.js
+++ part-2.js
@@ -1,169 +1,4 @@
-/*const buttonElement = document.getElementById("add-button");
-     const commentsElement = document.getElementById("comments");
-     const commentElement = document.getElementById("comment");
-     const nameInputElement = document.getElementById("name-input");
-     const textInputElement = document.getElementById("text-input");
-     //let commentsServer = [];
-     
-     const fetchPromise = fetch('https://wedev-api.sky.pro/api/v1/george-kuznetcov/comments',{
-         method: "GET",
-      });
-      fetchPromise.then((response) => {
-          const jsonPromise = response.json();
-          jsonPromise.then((responseData) =>{
-              console.log(responseData);
-              comments = responseData.comment
-              renderComments();
-          })
-      })
 
-     
-          function getDate(){
-       const dateNow = new Date();
-       const dateNumber = String(dateNow.getDate()).padStart(2, '0');
-       const dateMonth = String(dateNow.getMonth() + 1).padStart(2, '0');
-       const dateYear =dateNow.getFullYear()
-       const dateHours = dateNow.getHours()
-       const dateMinutes = dateNow.getMinutes()
-
-       return dateNumber + "." + dateMonth +"."+ dateYear + " "+ dateHours +":"+dateMinutes;
-     }
-
-     
-     const addNewElToList = () =>{
-
-       nameInputElement.classList.remove("error");
-       textInputElement.classList.remove("error");
-
-
-       
-       if (nameInputElement.value === "") {
-         nameInputElement.classList.add("error");
-         return;
-       }
-
-       if (textInputElement.value === "") {
-         textInputElement.classList.add("error");
-         return;
-       }
-
-       comments.push({
-         name: nameInputElement.value
-         .replaceAll("<", "&lt;")
-         .replaceAll("<", "&lt;"),
-         comment: textInputElement.value
-         .replaceAll("<", "&lt;")
-         .replaceAll("<", "&lt;"),
-         date: getDate(),
-         likes: 0,
-         isLiked: false
-       })
-
-     const fetchPromise = fetch('https://wedev-api.sky.pro/api/v1/george-kuznetcov/comments',{
-                 method: "POST",
-                 body: JSON.stringify({
-                  name: nameInputElement.value,
-                  text: textInputElement.value
-     })
-          });
-
-         fetchPromise.then((response) => {
-         const jsonPromise = response.json();
-         jsonPromise.then((responseData) =>{
-             console.log(responseData);
-             comments = responseData.comment
-             renderComments();
-         })
-     })
-
-       renderComments();
-
-     
-       nameInputElement.value = "";
-       textInputElement.value = "";
-       nameInputElement.placeholder = "Введите ваше имя";
-       textInputElement.placeholder = "Введите ваш комментарий";
-       //nameInputElement.blur();
-       //textInputElement.blur();
-
-     }
-
-     
-     function removeLastElement() {
-       comments.pop();
-       renderComments();
-       }
-     buttonElement.addEventListener("click", addNewElToList);
-
-     
-     const comments = []
-
-    
-     const renderComments = () =>{
-       const commentsHTML = comments
-      .map((comment, index) => {
-        return `<li id= "comment" class="comment" data-index="${index}">
-        <div class="comment-header">
-          <div class= "comment-name" data-name="${comment.author.name}">${comment.author.name}</div>
-          <div>${getDate(comment.date)}</div>
-         </div>    
-         <div class="comment-body">
-           <div class="comment-text" data-text="${comment.text}">
-             ${comment.text}
-           </div>
-         </div>
-         <div class="comment-footer">
-           <div class="likes" data-index="${index}">
-             <span class="likes-counter" data-index="${index}">${comment.likes}</span>
-             <button class= "like-button ${comment.isLiked}" data-index="${index}"></button>
-           </div>
-         </div>
-      </li>`;
-
-   }).join("");
-
-
-       const commentsEl = document.getElementById("comments");
-
-      commentsEl.innerHTML = commentsHTML;
-
-
-       const usersLikes = document.querySelectorAll('.likes');
-       const numberLikesEl = document.querySelectorAll('.likes-counter');
-       const likesPainter = document.querySelectorAll('.like-button')
-
-       
-       for (let userLike of usersLikes ) {
-         userLike.addEventListener('click',(event) => {
-           event.stopPropagation();
-           const indexUserLike = userLike.dataset.index;
-
-           if (comments[indexUserLike].isLiked === '') {
-             comments[indexUserLike].likes += 1;
-             comments[indexUserLike].isLiked = '-active-like';
-           } else{
-             comments[indexUserLike].likes -= 1;
-             comments[indexUserLike].isLiked = '';
-           }
-          renderComments();
-
-         })
-       }
-
-      
-       const responseUsersComments = document.querySelectorAll('.comment');
-       for(const responseUserComment of responseUsersComments ){
-         responseUserComment.addEventListener('click',() => {
-           const userName = comments[responseUserComment.dataset.index].name;
-           const userText = comments[responseUserComment.dataset.index].comment;
-           textInputElement.value = '>'+ "  " + `${userText}` + '\n' +'('+ `${userName}`+')' + '\n'; 
-         })
-       }
-
-     }
-
-     renderComments();
-     */
     const buttonElement = document.getElementById("add-button");
     const commentsElement = document.getElementById("comments");
     const commentElement = document.getElementById("comment");
@@ -173,8 +8,8 @@
     
 
     function getComments() {
-  const fetchPromise = fetch('https://webdev-hw-api.vercel.app/api/v1/george-kuznecov/comments', {
-    method: "GET",
+      const fetchPromise = fetch('https://webdev-hw-api.vercel.app/api/v1/george-kuznecov/comments', {
+        method: "GET",
   });
 
   fetchPromise.then((response) => {
@@ -227,43 +62,69 @@
        })
 
 
-      const fetchPromise = fetch('https://webdev-hw-api.vercel.app/api/v1/george-kuznecov/comments', {
-        method: "POST",
-        body: JSON.stringify({
-          name: nameInputElement.value,
-          text: textInputElement.value
-        }),
-      });
+       buttonElement.disabled = true;
+       buttonElement.textContent = 'Комментарий добавляется, подождите...';
+       const fetchPromise = fetch('https://webdev-hw-api.vercel.app/api/v1/george-kuznecov/comments', {
+         method: "POST",
+         body: JSON.stringify({
+           name: nameInputElement.value,
+           text: textInputElement.value
+         }),
+       })
+       .then((response) => {
+       })
+       .then((responseData) => {
+         getComments();
+         renderComments();
+       })
+       .then (() =>{
+         buttonElement.disabled = false;
+         buttonElement.textContent = 'Написать';
+         nameInputElement.value = "";
+         textInputElement.value = "";
+         nameInputElement.placeholder = "Введите ваше имя";
+         textInputElement.placeholder = "Введите ваш комментарий";
+       })
+       
+       renderComments();
+ 
+     }
+   
+     buttonElement.addEventListener("click",() => {
+       addNewElToList();
+       
+     });
+ 
+    
+     function removeLastElement() {
+       comments.pop(); 
+       renderComments(); 
+     }
+      
+     
+ 
+   
 
-      fetchPromise.then((response) => {
-        const jsonPromise = response.json();
-        jsonPromise.then((responseData) => {
-          getComments();
-          renderComments(commentsServer);
-          console.log(commentsServer);
-        });
-      });
+      //const fetchPromise = fetch('https://webdev-hw-api.vercel.app/api/v1/george-kuznecov/comments', {
+      //  method: "POST",
+      //  body: JSON.stringify({
+      //    name: nameInputElement.value,
+      //    text: textInputElement.value
+      //  }),
+    //  });
+
+   //   fetchPromise.then((response) => {
+     //   const jsonPromise = response.json();
+    //    jsonPromise.then((responseData) => {
+     //     getComments();
+          //renderComments(commentsServer);
+          //console.log(commentsServer);
+      //  });
+    //  });
 
 
 
-      nameInputElement.value = "";
-      textInputElement.value = "";
-      nameInputElement.placeholder = "Введите ваше имя";
-      textInputElement.placeholder = "Введите ваш комментарий";
-
-    }
-
-    buttonElement.addEventListener("click",() => {
-      addNewElToList();
-
-    });
-
-
-    function removeLastElement() {
-      comments.pop();
-      renderComments();
-    }
-
+  
 
 
 
